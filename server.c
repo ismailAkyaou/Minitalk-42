974885d7b85225b08e121d792e2e74633c93b382
iAkyaou <116098578+ismailAkyaou@users.noreply.github.com>
2023-03-14 22:41:13 +0100

    Update server.c

--- server.c
+++ server.c
@@ -14,28 +14,28 @@
 
 void	ft_check(int *save, char *byte, int *ch, int *i)
 {
-	if (*save != *ch)
+	if (*save != *new_pid)
 	{
-		*save = *ch;
+		*save = *new_pid;
 		*byte = 0;
 		*i = 0;
 	}
 }
 
-void	sighandel(int sig, siginfo_t *info, void *noting)
+void	sighandel(int signal, siginfo_t *info, void *noting)
 {
 	static int	i;
 	static char	byte;
 	int			bit;
 	static int	save;
-	static int	ch;
+	static int	new_pid;
 
 	(void) noting;
 	if (!save)
 		save = info->si_pid;
-	ch = info->si_pid;
-	ft_check(&save, &byte, &ch, &i);
-	if (sig == SIGUSR1)
+	new_pid = info->si_pid;
+	ft_check(&save, &byte, &new_pid, &i);
+	if (signal == SIGUSR1)
 		bit = 1;
 	else
 		bit = 0;
